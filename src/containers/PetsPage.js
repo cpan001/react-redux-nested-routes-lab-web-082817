0065ae99bc2db720f1316ecaa958cf80ac5b1d7d
cpan001 <github email address>
2017-11-18 16:01:46 -0500

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import { fetchPets } from '../actions';
-import PetsNew from './PetsNew';
-import PetsShow from './PetsShow';
-import PetsList from '../components/PetsList';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { fetchPets } from "../actions";
+import PetsNew from "./PetsNew";
+import PetsShow from "./PetsShow";
+import PetsList from "../components/PetsList";
 
 class PetsPage extends Component {
-
   componentDidMount() {
     this.props.fetchPets();
   }
 
   render() {
+    console.log("in petspage", this.props.pets);
     return (
-      <div>Pets Page</div>
-    )
+      <div>
+        <div>Pets Page</div>
+        <Switch>
+          <Route path={`${this.props.match.url}/new`} component={PetsNew} />
+          <Route path={`${this.props.match.url}/:petId`} component={PetsShow} />
+          <Route
+            exact
+            path={`${this.props.match.url}`}
+            render={() => <PetsList pets={this.props.pets} />}
+          />
+        </Switch>
+      </div>
+    );
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
     pets: state.pets
   };
-}
+};
 
 export default connect(mapStateToProps, { fetchPets })(PetsPage);
--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-const PetsShow = ({ pet }) => 
-  <div className="col-md-8">
-    <h2>{pet.name}</h2>
-    <p>{pet.description}</p>
-  </div>;
+const PetsShow = ({ pet }) => {
+  console.log("in show");
+  return (
+    <div className="col-md-8">
+      <h2>{pet.name}</h2>
+      <p>{pet.description}</p>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    pet: {}
-  };
+  const petId = ownProps.match.params.petId;
+  const pet = state.pets.find(pet => pet.id == petId);
+  if (pet) {
+    return { pet };
+  } else {
+    return { pet: {} };
+  }
 };
 
 export default connect(mapStateToProps)(PetsShow);
